b58b06086932c4951c7a72845f31628d6b0617eb
Herman Moreno <inher88@gmail.com>
2012-02-05 18:23:25 -0600

    Rails empty application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TacoManager::Application.config.secret_token = '717312183188ef41d3bdc86df83fd01c85d8377aa628cc51d4bb1bc6e700b9e8914f58c8b9d0f9bf20474a4e8d3ea1d959663e64b70deab8c1f438a849b0deb6'
